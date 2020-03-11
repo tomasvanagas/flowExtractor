00504afb9d4d3df7223f8027dcc0052bedb63c2a
Tomas Vanagas <tomasvanagas@ymail.com>
2020-03-11 21:08:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,9 @@
 #include <netinet/in.h>
 #include <net/ethernet.h>
 
-char myIp[] = "192.168.0.116";
+char myIp[16];
 
 
-
-
-
-
-/* This function can be used as a callback for pcap_loop() */
 void everyPacketHandler(u_char *args, const struct pcap_pkthdr* header, const u_char* packet) {
     // https://www.devdungeon.com/content/using-libpcap-c
     struct ether_header *eth_header;
@@ -47,7 +42,8 @@ void everyPacketHandler(u_char *args, const struct pcap_pkthdr* header, const u_
     payload_length = header->caplen - (ethernet_header_length + ip_header_length + tcp_header_length);
     payload = packet + total_headers_size;
 
-    char srcIp[15], dstIp[15];    
+    char srcIp[16]; 
+    char dstIp[16]; 
     sprintf(srcIp, "%d.%d.%d.%d", (unsigned char)(ip_header[12]), (unsigned char)(ip_header[13]), (unsigned char)(ip_header[14]), (unsigned char)(ip_header[15]));
     sprintf(dstIp, "%d.%d.%d.%d", (unsigned char)(ip_header[16]), (unsigned char)(ip_header[17]), (unsigned char)(ip_header[18]), (unsigned char)(ip_header[19]));
     uint16_t srcPort = uint16_t((unsigned char)(tcp_header[0]) << 8 | (unsigned char)(tcp_header[1]));
@@ -56,22 +52,25 @@ void everyPacketHandler(u_char *args, const struct pcap_pkthdr* header, const u_
 
 
     if (payload_length > 0) {
+
         // SSH
         if(memcmp(payload, "SSH-", 4) == 0){
             if(memcmp(myIp, dstIp, sizeof(myIp)) == 0) {
                 char sshVersion[256];
+                memset(&sshVersion, 0, sizeof(sshVersion));
 
-                printf("%s:%d\n", srcIp, srcPort);
-                printf("%s:%d\n", dstIp, dstPort);
+                //printf("%s:%d\n", srcIp, srcPort);
+                //printf("%s:%d\n", dstIp, dstPort);
 
                 const u_char *temp_pointer = payload;
                 int byte_count = 0;
-                while (byte_count++ < payload_length) {
-                    if((uint8_t)*temp_pointer != 13) {
-                        temp_pointer++;
+                while (byte_count < payload_length && byte_count < sizeof(sshVersion)) {
+                    if((uint8_t)*temp_pointer == 13 || (uint8_t)*temp_pointer == 10) {
+                        break;
                     }
                     else{
-                        break;
+                        temp_pointer++;
+                        byte_count++;
                     }
                 }
 
@@ -80,26 +79,37 @@ void everyPacketHandler(u_char *args, const struct pcap_pkthdr* header, const u_
                 char dataToWrite[256];
                 sprintf(dataToWrite, "%s:%d:%s\n", srcIp, srcPort, sshVersion); 
 
-                FILE* pFile;
-                pFile = fopen("ssh.txt", "a");
+                FILE* pFile = fopen("ssh.txt", "a");
                 fwrite(dataToWrite, sizeof(char), strlen(dataToWrite), pFile);
                 fclose(pFile);
 
 
-                printf("%s", sshVersion);
+                //printf("%s", sshVersion);
                 //printf("%d ", (uint8_t)*temp_pointer);
-                printf("\n---------\n");
+                //printf("\n---------\n");
             }
 
         }
-        
+
+        // Bitcoin
+        if(memcmp(payload, "\xf9\xbe\xb4\xd9", 4) == 0){
+            return;
+        }
+
+
     }
 }
 
 int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("%s <interface> <yourIpAddress>", argv[0]);
+        return 1;
+    }
+    sprintf(myIp, "%s", argv[2]);
+
     pcap_t *handle;
     char error_buffer[PCAP_ERRBUF_SIZE];
-    char *device = "en1";
+    char *device = argv[1];
     int snapshot_len = 1028;
     int promiscuous = 0;
     int timeout = 1000;
