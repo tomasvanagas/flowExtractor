eb94ec82dffb60557b431d1d0252391a5172affb
Tomas Vanagas <tomasvanagas@ymail.com>
2020-03-11 23:39:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -59,9 +59,6 @@ void everyPacketHandler(u_char *args, const struct pcap_pkthdr* header, const u_
                 char sshVersion[256];
                 memset(&sshVersion, 0, sizeof(sshVersion));
 
-                //printf("%s:%d\n", srcIp, srcPort);
-                //printf("%s:%d\n", dstIp, dstPort);
-
                 const u_char *temp_pointer = payload;
                 int byte_count = 0;
                 while (byte_count < payload_length && byte_count < sizeof(sshVersion)) {
@@ -82,21 +79,67 @@ void everyPacketHandler(u_char *args, const struct pcap_pkthdr* header, const u_
                 FILE* pFile = fopen("ssh.txt", "a");
                 fwrite(dataToWrite, sizeof(char), strlen(dataToWrite), pFile);
                 fclose(pFile);
-
-
-                //printf("%s", sshVersion);
-                //printf("%d ", (uint8_t)*temp_pointer);
-                //printf("\n---------\n");
             }
 
         }
 
         // Bitcoin
-        if(memcmp(payload, "\xf9\xbe\xb4\xd9", 4) == 0){
+        if(memcmp(payload, "\xf9\xbe\xb4\xd9", 4) == 0) {
+            if(memcmp(myIp, dstIp, sizeof(myIp)) == 0) {
+                const u_char *tempPointer = payload + 4;
+                int tempLength = payload_length - 4;
+
+                // Version packet
+                if(memcmp(tempPointer, "version", 7) == 0 && tempLength >= 20) {
+                    tempPointer += 12;  tempLength -= 12;
+                    tempPointer += 8;   tempLength -= 8; // Skip payload length and checksum
+
+                    if(tempLength >= 4) {
+                        char userAgent[17]; memset(&userAgent, 0, sizeof(userAgent));
+                        char relayFlag[3];  memset(&relayFlag, 0, sizeof(relayFlag));
+                        unsigned int protocolVersion = (unsigned int)( (unsigned char)(tempPointer[3]) << 24 | (unsigned char)(tempPointer[2]) << 16 | (unsigned char)(tempPointer[1]) << 8 | (unsigned char)(tempPointer[0]) );
+                        tempPointer += 4; tempLength -= 4;
+
+                        tempPointer += 8;   tempLength -= 8; // Skip node services
+                        tempPointer += 8;   tempLength -= 8; // Skip timestamp
+                        tempPointer += 26;  tempLength -= 26; // Skip reveiving address
+                        tempPointer += 26;  tempLength -= 26; // Skip emiting address
+                        tempPointer += 8;   tempLength -= 8; // Skip random nounce
+
+                        // User agent
+                        if(tempLength >= 1) {
+                            unsigned char userAgentLength = (unsigned char)(tempPointer[0]);
+                            tempPointer += 1; tempLength -= 1;
+                            if(tempLength >= userAgentLength) {
+                                memcpy(userAgent, tempPointer, userAgentLength);
+                                tempPointer += 16; tempLength -= 16;
+
+                                tempPointer += 4; tempLength -= 4; // Skip last block
+
+                                // Relay flag
+                                if(tempLength >= 1) {
+                                    sprintf(relayFlag, "%.2X", tempPointer[0]);
+                                }
+                            }
+                        }
+
+
+                        char dataToWrite[256];
+                        sprintf(dataToWrite, "%s:%d:%d:%s:%s\n", srcIp, srcPort, protocolVersion, userAgent, relayFlag); 
+                        FILE* pFile = fopen("bitcoin.txt", "a");
+                        fwrite(dataToWrite, sizeof(char), strlen(dataToWrite), pFile);
+                        fclose(pFile);
+                    }
+                }      
+
+                /*
+                for(int i=0; i<4; i++) {
+                    printf("%.2X ", (unsigned char)(tempPointer[0 + i]));
+                }
+                */
+            }
             return;
         }
-
-
     }
 }
 
